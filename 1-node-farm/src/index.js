f6382b5a600224245eb4029f5fa4a33f5e59600c
mitendraanand <mitendra@gmail.com>
2020-02-29 02:07:11 +0530

    sync, async, fs, http, webser

---
+++ 1-node-farm/src/index.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const http = require('http');
+
+///////////////////////////////////////////////////////////////////////
+//// FILES
+
+// Blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreate on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File Written!')
+
+// Non-Blocking, synchronous way{
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     if (err) return console.log('ERROR!!!')
+
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//             console.log(data3);
+
+//             fs.writeFile('./txt/fianl.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                 console.log('Your file has been writte!')
+//             })
+//         });        
+//     });
+// });
+// console.log('will read file!');
+
+
+///////////////////////////////////////////////////////////////////////
+//// SERVER
+const server = http.createServer((req, res) => {
+    //console.log(req);
+    res.end('Hello from the server!');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to request on port 8000');
+});
