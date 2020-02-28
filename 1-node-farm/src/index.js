d26694279d3c287606630304125ba01d9f9b8e2a
mitendraanand <mitendra@gmail.com>
2020-02-29 02:50:52 +0530

    simple routing!

--- 1-node-farm/src/index.js
+++ 1-node-farm/src/index.js
@@ -32,6 +32,10 @@ const url = require('url');
 
 ///////////////////////////////////////////////////////////////////////
 //// SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
     //console.log(req.url);
     const pathName = req.url;
@@ -39,6 +43,9 @@ const server = http.createServer((req, res) => {
         res.end('This is the OVERVIEW');
     } else if (pathName === '/product') {
         res.end('This is the PRODUCT');
+    } else if (pathName === '/api') {
+            res.writeHead(200, {'Content-type': 'application/json'});
+            res.end(data);
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
