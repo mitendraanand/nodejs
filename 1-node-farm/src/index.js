feac39069ac12f0a980164b528410da90c1d5a43
mitendraanand <mitendra@gmail.com>
2020-02-29 14:31:45 +0530

    dynamic website

--- 1-node-farm/src/index.js
+++ 1-node-farm/src/index.js
@@ -33,6 +33,24 @@ const url = require('url');
 ///////////////////////////////////////////////////////////////////////
 //// SERVER
 
+const replaceTemplate = (temp, product) => {
+    // let allows to mutate after it's been created.
+    let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS_NAME%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    
+    if(!product.organic) {
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+        console.log(output)
+    }
+    return output;
+}
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
@@ -50,9 +68,10 @@ const server = http.createServer((req, res) => {
     if (pathName === '/' || pathName === '/overview') {
         res.writeHead(200, {'Content-type': 'text/html'});
 
-        const cardHTML = ataObj.map(el => replaceTemplate(tempCard, el));
+        const cardHTML = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardHTML);
         
-        res.end(tempOverview);
+        res.end(output);
 
     // Product page
     } else if (pathName === '/product') {
