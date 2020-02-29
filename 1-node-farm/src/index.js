d216c81120b5766c35d4b8f097137cfd3fa7253c
mitendraanand <mitendra@gmail.com>
2020-02-29 19:10:26 +0530

    prettier extension

--- 1-node-farm/src/index.js
+++ 1-node-farm/src/index.js
@@ -6,8 +6,6 @@ const slugify = require('slugify'); // changes the last part of URL with unique 
 
 const replaceTemplate = require('./modules/replaceTemplate');
 
-
-
 ///////////////////////////////////////////////////////////////////////
 //// FILES
 
@@ -30,66 +28,71 @@ const replaceTemplate = require('./modules/replaceTemplate');
 //             fs.writeFile('./txt/fianl.txt', `${data2}\n${data3}`, 'utf-8', err => {
 //                 console.log('Your file has been writte!')
 //             })
-//         });        
+//         });
 //     });
 // });
 // console.log('will read file!');
 
-
 ///////////////////////////////////////////////////////////////////////
 //// SERVER
-
-
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
 
-const slugs = dataObj.map(el => slugify(el.productName, {lower: true}))
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
 console.log(slugs);
 
 const server = http.createServer((req, res) => {
-    //console.log(req.url);
-    //const pathName = req.url;
-    const {query, pathname} = url.parse(req.url, true);
+  //console.log(req.url);
+  //const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
-    // Overview page
-    if (pathname === '/' || pathname === '/overview') {
-        res.writeHead(200, {'Content-type': 'text/html'});
+  // Overview page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
 
-        const cardHTML = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
-        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardHTML);
-        
-        res.end(output);
+    const cardHTML = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardHTML);
+
+    res.end(output);
 
     // Product page
-    } else if (pathname === '/product') {
-        //console.log(query)
-        res.writeHead(200, {'Content-type': 'text/html'});
-        const product = dataObj[query.id];
-        const output = replaceTemplate(tempProduct, product);
+  } else if (pathname === '/product') {
+    //console.log(query)
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
 
-        res.end(output);
+    res.end(output);
 
     // API page
-    } else if (pathname === '/api') {
-            res.writeHead(200, {'Content-type': 'application/json'});
-            res.end(data);
+  } else if (pathname === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
 
     // Not Found
-    } else {
-        res.writeHead(404, {
-            'Content-type': 'text/html',
-            'my-own-header': 'hello-world'
-        });
-        res.end('<h1>Page not found!</h1>');
-    }
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
 });
 
 server.listen(8000, '127.0.0.1', () => {
-    console.log('Listening to request on port 8000');
+  console.log('Listening to request on port 8000');
 });
-
