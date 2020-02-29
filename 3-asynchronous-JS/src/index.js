e2b45d10c83dad8885540b5255b402a49e07edd3
mitendraanand <mitendra@gmail.com>
2020-03-01 01:45:16 +0530

    prmises

--- 3-asynchronous-JS/src/index.js
+++ 3-asynchronous-JS/src/index.js
@@ -2,17 +2,35 @@ const fs = require('fs');
 const superagent = require('superagent');
 
 // CALLBACK HELL
+// fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+//   console.log(`Breed: ${data}`);
+
+//   superagent
+//     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     .end((err, res) => {
+//       if (err) return console.log(err.message);
+//       console.log(res.body.message);
+
+//       fs.writeFile('dog-img.txt', res.body.message, err => {
+//         console.log('Random dog image save to file');
+//       });
+//     });
+// });
+
+// PROMISES
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
+    .then(res => {
       console.log(res.body.message);
 
       fs.writeFile('dog-img.txt', res.body.message, err => {
         console.log('Random dog image save to file');
       });
+    })
+    .catch(err => {
+      console.log(err.message);
     });
 });
