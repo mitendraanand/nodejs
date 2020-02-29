d6ccf396eebe97f8fdd7afe39e7305fe0e246fd0
mitendraanand <mitendra@gmail.com>
2020-03-01 02:19:52 +0530

    Async/Await

--- 3-asynchronous-JS/src/index.js
+++ 3-asynchronous-JS/src/index.js
@@ -56,18 +56,39 @@ const writeFilePro = (file, data) => {
   });
 };
 
-readFilePro(`${__dirname}/dog.txt`)
-  .then(data => {
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then(data => {
+//     console.log(`Breed: ${data}`);
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then(res => {
+//     console.log(res.body.message);
+//     return writeFilePro('dog-img.txt', res.body.message);
+//   })
+//   .then(() => {
+//     console.log('Random dog image saved to file.');
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+// ASYNC and AWAIT from ES8 uses the PROMISE in even more cleaner way
+// JUST a SYNTAXTICAL SUGAR
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`); // No need of then, await does the trick
     console.log(`Breed: ${data}`);
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then(res => {
-    console.log(res.body.message);
-    return writeFilePro('dog-img.txt', res.body.message);
-  })
-  .then(() => {
+
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    console.log(res.body);
+
+    await writeFilePro('dog-img.txt', res.body.message);
     console.log('Random dog image saved to file.');
-  })
-  .catch(err => {
+  } catch (err) {
     console.log(err);
-  });
+  }
+};
+
+getDogPic();
