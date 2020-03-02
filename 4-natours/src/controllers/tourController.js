9858c28aea21e8080bab0a616ef21e778ccd623f
mitendraanand <mitendra@gmail.com>
2020-03-02 22:52:26 +0530

    select query implemented

--- 4-natours/src/controllers/tourController.js
+++ 4-natours/src/controllers/tourController.js
@@ -18,12 +18,20 @@ exports.getAllTours = async (req, res) => {
 
     let query = Tour.find(JSON.parse(queryStr));
 
-    // 1C) Sorting via chaining
+    // 2) Sorting via chaining
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
     } else {
       query = query.sort('-createdAt');
+    }
+
+    // 3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v')
     }
 
     //////// FOR REFERENCE //////////////////////////////
