aa2bba4f146b398f6ba0a18fbd24b32971404b48
mitendraanand <mitendra@gmail.com>
2020-03-03 00:19:17 +0530

    Refacotoring

--- 4-natours/src/controllers/tourController.js
+++ 4-natours/src/controllers/tourController.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
 
 exports.aliasTopTours = async (req, res, next) => {
   req.query.limit = '5';
@@ -9,75 +10,9 @@ exports.aliasTopTours = async (req, res, next) => {
   next();
 };
 
-class APIFeatures {
-  constructor(query, queryString) {
-    this.query = query; // query that finds all the documnents from the model
-    this.queryString = queryString; // part of request, after '?'
-  }
-
-  filter() {
-    // Basic Filtering
-    const queryObj = { ...this.queryString }; // ES 6 trick of creating deep copy of object for modification.
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-
-    excludedFields.forEach(el => delete queryObj[el]);
-
-    // Adavanced Filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-
-    this.query = this.query.find(JSON.parse(queryStr));
-
-    return this;
-  }
-
-  sort() {
-    if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.split(',').join(' ');
-      this.query = this.query.sort(sortBy);
-    } else {
-      this.query = this.query.sort('-createdAt');
-    }
-
-    return this;
-  }
-
-  limitFields() {
-    if (this.queryString.fields) {
-      const fields = this.queryString.fields.split(',').join(' ');
-      this.query = this.query.select(fields);
-    } else {
-      this.query = this.query.select('-__v');
-    }
-
-    return this;
-  }
-
-  paginate() {
-    // Pagination, skip records based on limit per page
-    const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    this.query = this.query.skip(skip).limit(limit);
-
-    return this;
-  }
-}
-
 // ROUTE HANDLERS
 exports.getAllTours = async (req, res) => {
   try {
-    //////// FOR REFERENCE //////////////////////////////
-    // advanced query = { difficulty:'easy', duration: { $gte: 5 } }
-
-    // const query = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
-    ////////////////////////////////////////////////////
-
     // BUILD QUERY
     const features = new APIFeatures(Tour.find(), req.query)
       .filter()
@@ -85,7 +20,6 @@ exports.getAllTours = async (req, res) => {
       .limitFields()
       .paginate();
 
-    //console.log(features.query);
     // EXECUTE QUERY
     const tours = await features.query;
 
@@ -103,6 +37,16 @@ exports.getAllTours = async (req, res) => {
       message: err
     });
   }
+
+      //////// FOR REFERENCE //////////////////////////////
+    // advanced query = { difficulty:'easy', duration: { $gte: 5 } }
+
+    // const query = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+    ////////////////////////////////////////////////////
 };
 
 exports.getTour = async (req, res) => {
