0e842a762ba75bb60d7d556f28ad53405a388624
mitendraanand <mitendra@gmail.com>
2020-03-02 18:45:51 +0530

    added all CRUD routers

--- 4-natours/src/controllers/tourController.js
+++ 4-natours/src/controllers/tourController.js
@@ -60,18 +60,39 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  // not doing the code as it's simple java script stuff to read the json file and update.
-  // simply sendign the response.
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
 
-  res
-    .status(200)
-    .json({ status: 'success', data: { tour: '<updated the tour...>' } });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  // not doing the code as it's simple java script stuff to read the json file and update.
-  // simply sendign the response.
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
 
-  res.status(204).json({ status: 'success', data: null });
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
