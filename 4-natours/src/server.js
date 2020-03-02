fd31c2e946685c7e89ef0bda672d1e19c1258eaa
mitendraanand <mitendra@gmail.com>
2020-03-02 17:27:23 +0530

    created tour schma and updated from express

--- 4-natours/src/server.js
+++ 4-natours/src/server.js
@@ -21,6 +21,39 @@ mongoose
     console.log('DB Connection Successfull');
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Hiker',
+  rating: 4.7,
+  price: 497
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log(`ERROR: ${err}`);
+  });
+
 // START THE SERVER
 const port = process.env.PROT || 3000;
 const app = require('./app');
