b26869ad709ea91a66c4ad4bbdd7847441bbf211
mitendraanand <mitendra@gmail.com>
2020-03-03 18:01:56 +0530

    unhandled exceptions...

--- 4-natours/src/server.js
+++ 4-natours/src/server.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+  console.log('uncaughtException. Shutting Down...');
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 //console.log(process.env);
 
