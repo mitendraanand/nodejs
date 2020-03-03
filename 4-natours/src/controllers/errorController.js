f9c5bd2ebdae9c7a42523f68379c1dee95be9cd5
mitendraanand <mitendra@gmail.com>
2020-03-03 17:40:46 +0530

    Update error

--- 4-natours/src/controllers/errorController.js
+++ 4-natours/src/controllers/errorController.js
@@ -5,6 +5,21 @@ const handleCastErrorDB = err => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldDB = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+}
+
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     error: err,
@@ -19,7 +34,8 @@ const sendErrorProd = (err, res) => {
   if (err.isOperational) {
     res.status(err.statusCode).json({
       error: err,
-      status: err.status
+      status: err.status,
+      message: err.message
     });
 
     // Programming or other unknown error: don't want to leak error details
@@ -49,6 +65,13 @@ module.exports = (err, req, res, next) => {
     if (error.name == 'CastError') {
       error = handleCastErrorDB(error);
     }
+    if (error.code === 11000) {
+      error = handleDuplicateFieldDB(error);
+    }
+    if (error.name === 'ValidationError') {
+      error = handleValidationErrorDB(error);
+    }
+
     sendErrorProd(error, res);
   }
 };
