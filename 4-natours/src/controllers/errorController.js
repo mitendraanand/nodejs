cf8145586ee9507f1a70e1a41d011b6206a32618
mitendraanand <mitendra@gmail.com>
2020-03-03 16:56:19 +0530

    Dev/Prod error

--- 4-natours/src/controllers/errorController.js
+++ 4-natours/src/controllers/errorController.js
@@ -1,3 +1,32 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    error: err,
+    status: err.status,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Oeprational, trusted error: send message to client
+  if (err.isOperationl) {
+    res.status(err.statusCode).json({
+      error: err,
+      status: err.status
+    });
+
+    // Programming or other unknown error: don't want to leak error details
+  } else {
+    // Log the error
+    console.error('ERROR', err);
+    // Send a generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'something went very error'
+    });
+  }
+};
+
 // By providing 4 parameter express already knows that this is ERROR
 // handling MIDDLEWARE
 module.exports = (err, req, res, next) => {
@@ -6,8 +35,9 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
