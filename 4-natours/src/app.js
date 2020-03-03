543adce013c3bfe7391d1d5297873d04b77ff36a
mitendraanand <mitendra@gmail.com>
2020-03-03 14:35:31 +0530

    global error handling

--- 4-natours/src/app.js
+++ 4-natours/src/app.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
 
+const appError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routers/tourRouters');
 const userRouter = require('./routers/userRouters');
 
@@ -28,30 +30,15 @@ app.use('/api/v1/users', userRouter); // Mounting the router
 
 // handling bad requests
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `can't find the ${req.originalUrl} on this server!`
-  // });
-  const err = new Error(`can't find the ${req.originalUrl} on this server!`);
-  err.status = 'fail';
-  err.statusCode = 404;
-
   // whenever we pass anything it will be assumed that there is ERROR.
   // all the middleware will be skipped and the ERROR MIDDLEWARE will be
   // executed.
-  next(err);
+  next(new appError(`can't find the ${req.originalUrl} on this server!`, 404));
 });
 
-// By providing 4 parameter express already knows that this is ERROR
-// handling MIDDLEWARE
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
+// globalErrorHandler has 4 parameters so express already knows that
+// this is ERROR handling MIDDLEWARE
+app.use(globalErrorHandler);
 
 module.exports = app;
 
---
+++ 4-natours/src/controllers/errorController.js
@@ -0,0 +1,13 @@
+// By providing 4 parameter express already knows that this is ERROR
+// handling MIDDLEWARE
+module.exports = (err, req, res, next) => {
+  //console.log(err.stack);
+
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
+};
