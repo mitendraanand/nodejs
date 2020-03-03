ae63db56c3505b0264970b8ab57a653fab31115a
mitendraanand <mitendra@gmail.com>
2020-03-03 10:27:19 +0530

    Virtual Properties

--- 4-natours/src/models/tourModel.js
+++ 4-natours/src/models/tourModel.js
@@ -1,68 +1,87 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-    trim: true
+////////// FAT MODEL and THIN CONTROLLER /////////////
+// PUT AS MUCH AS BUSINESS LOGIC AS POSSBILE IN MODEL
+// AND KEEP THE CONTROLLER AS THIN AS POSSIBLE WITH
+// ONLY APPLICATION LOGIC
+//////////////////////////////////////////////////////
+
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration']
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size']
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty']
+    },
+    ratingAverage: {
+      type: Number,
+      default: 4.5
+    },
+    ratingQuantity: {
+      type: Number,
+      default: 0
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price']
+    },
+    discount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a summary']
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image']
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false
+    },
+    startDates: [Date]
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration']
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size']
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty']
-  },
-  ratingAverage: {
-    type: Number,
-    default: 4.5
-  },
-  ratingQuantity: {
-    type: Number,
-    default: 0
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price']
-  },
-  discount: Number,
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a summary']
-  },
-  description: {
-    type: String,
-    trim: true
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image']
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false
-  },
-  startDates: [Date]
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+// virtual property is calculated everytime document is pulled from database
+// it doesn't exist in db.
+tourSchema.virtual('durationWeeks').get(function() {
+  return this.duration / 7; // arrow function does not get this key word so  regular function.
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
-const testTour = new Tour({
-  name: 'The Park Hiker',
-  rating: 4.7,
-  price: 497
-});
-
 module.exports = Tour;
+
+// const testTour = new Tour({
+//   name: 'The Park Hiker',
+//   rating: 4.7,
+//   price: 497
+// });
+
 // testTour
 //   .save()
 //   .then(doc => {
