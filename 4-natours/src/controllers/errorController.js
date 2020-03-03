dff06c05b869071b734bc4fd2f308e13b2d2a787
mitendraanand <mitendra@gmail.com>
2020-03-03 17:26:26 +0530

    Mongoose DB Error  handling ....

--- 4-natours/src/controllers/errorController.js
+++ 4-natours/src/controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     error: err,
@@ -9,7 +16,7 @@ const sendErrorDev = (err, res) => {
 
 const sendErrorProd = (err, res) => {
   // Oeprational, trusted error: send message to client
-  if (err.isOperationl) {
+  if (err.isOperational) {
     res.status(err.statusCode).json({
       error: err,
       status: err.status
@@ -38,6 +45,10 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    if (error.name == 'CastError') {
+      error = handleCastErrorDB(error);
+    }
+    sendErrorProd(error, res);
   }
 };
