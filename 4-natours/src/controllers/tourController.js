83569b0eaf1af49b0612fba69228b422fb50ce46
mitendraanand <mitendra@gmail.com>
2020-03-03 16:38:12 +0530

    Handling 404 Error

--- 4-natours/src/controllers/tourController.js
+++ 4-natours/src/controllers/tourController.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 // This is MIDDLEWARE as it has next as parameter.
 // Makes sense to have MIDDLEWARE because all we need is
@@ -46,9 +47,15 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
   ////////////////////////////////////////////////////
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
   // Tour.findOne({_id: req.params.id})
+
+  if (!tour) {
+    // Call to next() will take the control to globalErrorHandler()
+    // due to ERROR handling MIDDLEWARE in app.js
+    return next(new AppError('No tour found with that id', 404));
+  }
 
   res.status(200).json({
     status: 'success',
@@ -75,6 +82,12 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     runValidators: true
   });
 
+  if (!tour) {
+    // Call to next() will take the control to globalErrorHandler()
+    // due to ERROR handling MIDDLEWARE in app.js
+    return next(new AppError('No tour found with that id', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -84,7 +97,13 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) {
+    // Call to next() will take the control to globalErrorHandler()
+    // due to ERROR handling MIDDLEWARE in app.js
+    return next(new AppError('No tour found with that id', 404));
+  }  
 
   res.status(204).json({
     status: 'success',
